89adc1e2c100977bd7f6fe99baf14757c77ecbc7
dzmitry <dzmitry@Dzmitrys-MacBook-Air-old.local>
2021-09-12 22:37:21 +0400

    Read scheme settings

--- js/main.js
+++ js/main.js
@@ -9,6 +9,20 @@ window.onload = () => {
   // glightbox.js is a third-party library for image galleries.
   const lightbox = GLightbox();
 
+  // Read Dark Mode settings from the operating system level and apply it
+  function applyDarkSchemeSettings() {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    if (prefersDarkScheme.matches) {
+      document.body.classList.add("dark-theme");
+    } else {
+      document.body.classList.remove("dark-theme");
+    }
+  }
+
+  // Add an event listener for operating system color scheme setting update
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', applyDarkSchemeSettings);
+  applyDarkSchemeSettings();
+
   const darkModeToggle = document.querySelector("#darkModeToggle");
   darkModeToggle.addEventListener('click', function(e) {
     document.body.classList.toggle('dark-theme');
